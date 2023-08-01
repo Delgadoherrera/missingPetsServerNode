32ab6453f632692d54523fb0b0b086b0922ad6a1
Luciano Delgado <ldelgado@grupolpa.com>
2023-08-01 13:30:57 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+const  { expressjwt: jwt } = require("express-jwt");
+const jwks = require('jwks-rsa');
+const bodyParser = require("body-parser");
+const db = require("./database/models");
+const Mensaje = db.Mensaje;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const auth = require("./middlewares/auth");
+
+const session = require("express-session");
+const userApi = require("./api/userApi");
+const mascotaApi = require("./api/mascotaApi");
+const mensajesApi = require("./api/mensajesApi");
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dev-xxqnbow4.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'https://backend.missingpets.art/mascotas/mascotasPerdidas',
+issuer: 'https://dev-xxqnbow4.us.auth0.com/',
+algorithms: ['RS256']
+});
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+/* app.use(jwtCheck);
+ */
+
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+/* app.use(
+  session({
+    secret: "missingPetsssss",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+ */
+server.listen(4000);
+app.use(cors({ origin: "*" }));
+app.use("/", userApi);
+app.use("/", /* auth */ mascotaApi);
+app.use("/", mensajesApi);
+
+io.on("connection", (socket) => {
+  socket.on("message", (body, idEmisor, idReceptor) => {
+    console.log("DATOS DESDE APP", body, idEmisor, idReceptor);
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(8),
+    });
+    const objetoFecha = Date.now();
+    const nowDate = new Date(objetoFecha);
+    let fechaMensaje = nowDate.toLocaleDateString("en-ZA");
+
+    Mensaje.create({
+      mensaje: body,
+      emailEmisor: idEmisor,
+      emailReceptor: idReceptor,
+      fechaMensaje: fechaMensaje,
+    });
+  });
+});
+
+/* const PORT = process.env.port */
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log("servidor ON sen puerto: ", PORT);
+});
---
+++ src/database/models/Mensaje.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Mensaje';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        mensaje: {
+            type: dataTypes.STRING
+        },
+        fechaMensaje: {
+            type: dataTypes.DATE
+        },
+        emailEmisor: {
+            type: dataTypes.STRING
+        },
+        emailReceptor: {
+            type: dataTypes.STRING
+        },
+    };
+    let config = {
+        tableName: 'mensajes',
+        timestamps: false
+    };
+    const Mensaje = sequelize.define(alias, cols, config)
+
+    return Mensaje
+
+}
