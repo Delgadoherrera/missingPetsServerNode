9a62415bf7fc8d0b16cf9266809e795b1b7f1690
Luciano Delgado <ldelgado@grupolpa.com>
2023-09-02 01:26:53 -0300

    quitAdoption

--- src/api/mascotaApi.js
+++ src/api/mascotaApi.js
@@ -349,7 +349,20 @@ router.post("/mascotas/startAdoption/:id", async (req, res) => {
       },
     }
   );
-
+  res.status(200).send("success");
+});
+router.post("/mascotas/quitAdoption/:id", async (req, res) => {
+  console.log('req.param', req.params.id)
+  await Mascota.update(
+    {
+      status: 0,
+    },
+    {
+      where: {
+        idMascota: req.params.id,
+      },
+    }
+  );
   res.status(200).send("success");
 });
 
