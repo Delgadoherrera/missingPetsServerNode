f743d90e7fa9409c69845b19fa84f2541185f6f3
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-09-18 18:45:45 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 server.listen(4000);
-app.use(cors());
 app.use("/", userApi);
 app.use("/", mascotaApi);
 app.use("/", mensajesApi);
