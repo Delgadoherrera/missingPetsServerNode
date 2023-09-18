ac12df6c058f9394b26f868f4ca9933b3b9145b5
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-09-18 17:23:43 -0300

    app listen

--- src/app copy.js
+++ src/app copy.js
@@ -8,31 +8,28 @@ const jwks = require("jwks-rsa");
 const bodyParser = require("body-parser");
 const db = require("./database/models");
 const Mensaje = db.Mensaje;
-const http = require("http"); // Cambia 'require("http").createServer' a 'require("http")'
+const server = require("http").createServer(app);
 const userApi = require("./api/userApi");
 const mascotaApi = require("./api/mascotaApi");
 const mensajesApi = require("./api/mensajesApi");
-const io = require("socket.io"); // Cambia 'require("socket.io")(server, ...)' a 'require("socket.io")(http, ...)'
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
-
-const PORT = 3001; // Cambia el puerto a 4000 para WebSocket
-
-// Cambia 'server.listen(4000);' a 'http.createServer(app).listen(PORT);'
-http.createServer(app).listen(PORT);
-
+server.listen(4000);
 app.use("/", userApi);
 app.use("/", mascotaApi);
 app.use("/", mensajesApi);
 
-const socketServer = io(http); // Cambia 'io(server, ...)' a 'io(http, ...)'
-
-socketServer.on("connection", (socket) => {
-  socket.on("message", (body, idEmisor, idReceptor, nombreEmisor) => {
+io.on("connection", (socket) => {
+  socket.on("message", (body, idEmisor, idReceptor,nombreEmisor) => {
     console.log("DATOS DESDE APP", body);
     socket.broadcast.emit("message", {
       body,
@@ -47,10 +44,14 @@ socketServer.on("connection", (socket) => {
       emailEmisor: body.emailEmisor,
       emailReceptor: body.idReceptor,
       fechaMensaje: fechaMensaje,
-      nombreEmisor: body.nombreEmisor,
+      nombreEmisor: body.nombreEmisor
     });
   });
-  console.log("Socket.io connected"); // Agrega un registro de conexión para verificar
 });
 
-console.log("servidor ON en puerto:", PORT); // Mueve este registro fuera de la función de escucha de app
+/* const PORT = process.env.port */
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log("servidor ON sen puerto: ", PORT);
+});
